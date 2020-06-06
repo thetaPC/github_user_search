336bde8fe8ed74fce30b179ea7fd46bae030c8b5
Maria Loza <mlozacoder@gmail.com>
2020-06-06 02:10:00 -0700

    working on pagination

--- modules/custom/github_user_search/assets/js/user_search.js
+++ modules/custom/github_user_search/assets/js/user_search.js
@@ -9,7 +9,7 @@
     Promise.all([ajaxUserSearch()]).then(() => {
       // all requests finished successfully
       users.forEach((user, index) => {
-        console.log(user);
+        // console.log(user);
         $.ajax({
           url: "https://api.github.com/users/" + user,
           type: 'GET',
@@ -19,7 +19,6 @@
             client_secret: 'd1246418259eddc7b3b29e39759e3308af1e5fc1'
           },
           success: function (result) {
-            console.log(result);
             var column = document.createElement("div");
             column.className = "col col-xs-12";
             var img = document.createElement('img');
@@ -38,6 +37,21 @@
             biography.className = "biography";
             biography.innerText = result.bio;
             column.append(biography);
+            //
+            var follower_count = document.createElement("div");
+            follower_count.className = "follower-count";
+            follower_count.innerText = result.followers;
+            column.append(follower_count);
+            //
+            var profile_url = document.createElement("a");
+            profile_url.className = "profile-url";
+            profile_url.href = result.html_url;
+            profile_url.innerText = 'Visit Profile';
+            var profile_url_accessibility = document.createElement("span");
+            profile_url_accessibility.className = "sr-only";
+            profile_url_accessibility.innerText = ' of ' + result.login;
+            profile_url.append(profile_url_accessibility);
+            column.append(profile_url);
             //
             $("#user-search-results").append(column);
           }
@@ -65,11 +79,27 @@
             $user_count_text = 'user found.'
           }
           $("#user-search-results-count").append($user_count + ' ' + $user_count_text);
-          console.log(result);
+          // console.log(result);
           $("#user-search-results").empty();
           $.each(result.items, function (key, value) {
             users.push(value.login);
           });
+          // Pagination
+          $total_users_per_page = 30;
+          $total_pages = Math.ceil($user_count / $total_users_per_page);
+          // console.log($total_pages);
+          var pagination = document.createElement("ul");
+          pagination.className = "pagination";
+          for (i = 1; i < $total_pages; i++) {
+            var pagination_item = document.createElement("li");
+            var pagination_item_link = document.createElement("a");
+            // pagination_item_link.href = "https://api.github.com/search/users?q=" + data + '&page=' + i;
+            pagination_item_link.dataset.page = i;
+            pagination_item_link.innerText = i;
+            pagination_item.append(pagination_item_link);
+            pagination.append(pagination_item);
+          }
+          $("#user-search-results-count").append(pagination);
         }
       });
     }
